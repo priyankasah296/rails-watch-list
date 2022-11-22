49144fdda09ab830e97b42b5c26f48e4595cef6a
Priyanka <priyankasah296@gmail.com>
2022-11-22 20:37:26 +1100

    added env file to gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "lists#index"
-  resources :lists, except: [:index] do
+  resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: :destroy
